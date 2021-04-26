5e958a27fb1cabf7ffbdc1afd1221ae319a2373c
k4keye <vps32@naver.com>
2021-04-27 00:23:56 +0900

    grid

---
+++ RetrofitApp/app/src/main/java/com/example/retrofitapp/recyclerview/PhotoGridRecyclerViewAdapter.kt
@@ -0,0 +1,6 @@
+package com.example.retrofitapp.recyclerview
+
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoGridRecyclerViewAdapter : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+}
---
+++ RetrofitApp/app/src/main/java/com/example/retrofitapp/recyclerview/PhotoItemViewHolder.kt
@@ -0,0 +1,8 @@
+package com.example.retrofitapp.recyclerview
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class PhotoItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
