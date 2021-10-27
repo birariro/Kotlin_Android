500d832336e43a369ff0588571e0a33f5718f9bc
k4keye <vps32@naver.com>
2021-10-27 19:25:52 +0900

    firebase init

--- PushFireBaseSample/app/src/main/java/com/example/pushfirebasesample/MainActivity.kt
+++ PushFireBaseSample/app/src/main/java/com/example/pushfirebasesample/MainActivity.kt
@@ -2,13 +2,28 @@ package com.example.pushfirebasesample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.TextView
+import com.google.firebase.messaging.FirebaseMessaging
 
 class MainActivity : AppCompatActivity() {
     private val resultTextView : TextView by lazy { findViewById(R.id.resultTextView) }
-    private val firebaseToken : TextView by lazy { findViewById(R.id.resultTextView) }
+    private val firebaseToken : TextView by lazy { findViewById(R.id.firebaseTokenTextView) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initFirebase()
+
+    }
+    private fun initFirebase(){
+        //파이어 베이스 토큰 얻기
+        FirebaseMessaging.getInstance().token.addOnCompleteListener {
+                task ->
+            if(task.isSuccessful){
+                val token = task.result
+                firebaseToken.text = token
+                Log.d("k4keye","token :"+token);
+            }
+        }
     }
 }
